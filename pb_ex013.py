ac0e8066152d168b8dec3b9c2fcf88f0235f3d0a
Robson Vieira <87854460+tecvieira@users.noreply.github.com>
2021-07-25 14:35:26 -0300

    Add files via upload
    Veja seu peso ideal de forma rápida. Exercício 13.

---
+++ pb_ex013.py
@@ -0,0 +1,6 @@
+print('-' * 15)
+print('PESO IDEAL'.center(15))
+print('-' * 15)
+altura = float(input('Informe sua altura: '))
+peso = (72.7 * altura) - 58
+print(f'O seu peso ideal é {peso:.2f}Kg')
