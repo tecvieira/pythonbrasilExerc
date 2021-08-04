2cfef49bbc50b7a4d12beaf9c1b9f5ae3e82ec13
Robson Vieira <tecvander.vieira@gmail.com>
2021-08-04 06:32:37 -0300

    acrescentou nota de avaliação

--- ED014a.py
+++ ED014a.py
@@ -5,7 +5,15 @@ def media():
 
 def resultado() ->str:
     if media() >= 9:
-        return 'APROVADO'
+        return 'NOTA - A, (APROVADO)'
+    if 7.5 <= media() < 9:
+        return 'NOTA - B, (APROVADO)'
+    if 6.0 <= media() <7.5:
+        return 'NOTA - C, (APROVADO)'
+    if 4.0 <= media() < 6.0:
+        return 'NOTA - D, (REEPROVADO)'
+    if 0 <= media() <4.0:
+        return 'NOTA - E, (REPROVADO)'
 
 
 print('*'*45)
@@ -16,5 +24,5 @@ print('-'*45)
 nome = str(input('Nome do aluno: ')).strip().upper()
 nota1 = float(input('Nota do primeiro semestre: '))
 nota2 = float(input('Nota do segundo semestre:'))
-print(f'O aluno {nome}, com notas {nota1} e {nota2} obteve média {media()}.')
-print(f'O aluno está {resultado()}')
+print(f'O aluno {nome}, com notas {nota1} e {nota2:.2f} obteve média {media()}.')
+print(f'O aluno com {resultado()}.')
