7d90d60dd14906997435afcb9392653bc650c7ce
Robson Vieira <tecvander.vieira@gmail.com>
2021-08-09 22:19:25 -0300

    Analisando equação do segundo gráu. ok

--- ED016.py
+++ ED016.py
@@ -1,5 +1,3 @@
-
-
 print('-'*55)
 print('Calculando equação do segundo gráu:'.center(55))
 print('Para o modelo abaixo, informe valores para a, b, c.'.center(55))
@@ -13,26 +11,30 @@ while True:
         break
     b = int(input('Informe o valor de "b" :'))
     c = int(input('Informe o valor de "c" :'))
-    d = (b ** 2 - 4 * a * c)
+    d = (b ** 2 - (4 * a * c))
     x1 = (-b + d ** (1 / 2)) / (2 * a)
     x2 = (-b - d ** (1 / 2)) / (2 * a)
     if d < 0:
-        print(f'Valor de x1: {x1}\n')
-        print(f'Valor de x2: {x2}\n')
-        print(f'Para este valor {d}, negativo esta equação não possui raizes reais.')
+        print(f'Valor de x1: {x1:.2f}')
+        print(f'Valor de x2: {x2:.2f}')
+        print(f'Para um DELTA {d}, (NEGATIVO) esta equação não possui raizes reais.')
         break
     elif d == 0:
-        print(f'Valor de x1: {x1}\n')
-        print(f'Valor de x2: {x2}\n')
-        print(f'Para este valor {d}, esta equação possui duas raizes reais.')
+        print(f'Valor de x1: {x1}')
+        print(f'Valor de x2: {x2}')
+        print(f'Para um DELTA = {d}, esta equação possui duas raizes reais.')
         break
     else:
-        print(f'Valor do delta {d}\n ')
-        print(f'Valor de x1: {x1}\n')
+        print(f'Valor do delta {d} ')
+        print(f'Valor de x1: {x1}')
         print(f'Valor de x2: {x2}')
+    pergunta = str(input('Deseja continuar? [S/N}')).strip().upper()[0]
+    if pergunta == 'N':
+        break
+print('Até logo...')
 
 
-#corrigir as saidas
+
 
 
 
