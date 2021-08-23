b57c7297231915f8cb3c76b002b676a634ac1a42
Robson Vieira <tecvander.vieira@gmail.com>
2021-08-23 20:14:01 -0300

    número por extenso entre 0 e 999. Com desejo de continuar ou não. (Erro para número com zero no fim)

--- ED019.py
+++ ED019.py
@@ -9,27 +9,31 @@ dezenas = ['', '', 'vinte', 'trinta', 'quarenta', 'cinquenta',
 centenas = ['', 'cento', 'duzentos', 'trezentos', 'quatrocentos',
             'quinhentos', 'seiscentos', 'setecentos', 'oitocentos', 'novecentos']
 while True:
-    numero = int(input('\033[34mDigite o número: \033[m'))
-    if numero < 0 or numero > 999:
-        print('\033[31mERRO! digite um número entre 0 e 999.\033[m')
+    pergunta = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    if pergunta != 'S':
+        break
     else:
-        print(f'O número {numero} por extenso fica: ', end='')
-        if numero == 100:
-            print('cem')
-        elif numero == 0:
-            print('zero')
+        numero = int(input('\033[34mDigite o número: \033[m'))
+        if numero < 0 or numero > 999:
+            print('\033[31mERRO! digite um número entre 0 e 999.\033[m')
         else:
-            if numero > 100:
-                print(centenas[int(str(numero)[0])], end='')
-                numero = numero - int(str(numero)[0])*100
+            print(f'O número {numero} por extenso fica: ', end='')
+            if numero == 100:
+                print('cem')
+            elif numero == 0:
+                print('zero')
+            else:
+                if numero > 100:
+                    print(centenas[int(str(numero)[0])], end='')
+                    numero = numero - int(str(numero)[0])*100
+                    if numero != 0:
+                        print(' e', end=' ')
+                if numero < 20:
+                    print(menor_20[int(str(numero)[0])], end=' ')
+                    numero = 0
+                elif numero >= 20:
+                    print(dezenas[int(str(numero)[0])], end=' ')
+                    numero = numero - int(str(numero)[0])*10
                 if numero != 0:
-                    print(' e', end=' ')
-            if numero < 20:
-                print(menor_20[int(str(numero)[0])], end=' ')
-                numero = 0
-            elif numero >= 20:
-                print(dezenas[int(str(numero)[0])], end=' ')
-                numero = numero - int(str(numero)[0])*10
-            if numero != 0:
-                print('e', menor_20[numero])
-
+                    print('e', menor_20[numero])
+print('Fim...')
